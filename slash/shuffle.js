75d512500ac99c91ea9093263eff23ac824fee66
Shenkrad <58443488+Shenkrad@users.noreply.github.com>
2024-02-06 20:21:52 +0100

    Fixed shuffle command

--- slash/shuffle.js
+++ slash/shuffle.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!queue)
             return await interaction.editReply("There are no songs in the queue")
 
-        queue.shuffle()
-        await interaction.editReply(`The queue of ${queue.tracks.length} songs have been shuffled!`)
+        queue.toggleShuffle()
+        await interaction.editReply(`The queue of ${queue.tracks.size} songs have been shuffled!`)
     }
 }
